a3d45fccdee8a2f0997462528b0f1e67647b5afe
dipinbajra <bajradipin98@gmail.com>
2022-05-31 17:36:25 +0545

    initial commit with the creation of contactList as well as display table and database

---
+++ edit.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Phone Book</title>
+</head>
+<body>
+    <header>
+        <h1>Contact Application</h1>
+    </header>
+
+    <h2>Update Contact</h2>
+    <?php
+    include 'db.php';
+    $id = $_GET['id'];
+ 
+    $sql = "SELECT * FROM names WHERE id=".$id;
+
+    
+    $result=mysqli_query($conn, $sql);
+    
+    if ($result){
+        $row= mysqli_fetch_assoc($result);
+        $contactname= $row['name'];
+        $contactphone= $row['phone'];
+    }
+    ?>
+
+
+    <form action="editaction.php" method="post">
+        <div class="main">
+            <label for="name">Name:</label> <br>
+            <input type="text" name="name" id="name" value="<?php global $contactname; echo $contactname ?>" required > <br> <br>
+            <label for="contact">Number:</label> <br>
+            <input type="text" name="contact" id="contact" value="<?php global $contactphone; echo $contactphone?>" required > <br> <br>
+            <input type="hidden" name="id" id="id" value="<?php global $id; echo $id ?>" required >
+            <input type="submit" value="Update">
+        </div>
+    </form>
+    <hr>
+   
+</body>
+</html>
